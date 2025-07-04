15e2d8b1c0017f1351b979cfc134a8dde89f4d7c
Thomas Bryce Kelly <tbrycekelly@gmail.com>
2025-07-04 10:33:57 -0800

    Fix ResNet152 model

--- dpi_fun/ResNet152.py
+++ dpi_fun/ResNet152.py
@@ -74,9 +74,7 @@ def make_advanced_block_base(inputs, filter_num, stride=1):
     x = tf.keras.layers.Conv2D(filters=filter_num*4, kernel_size=(1, 1), strides=1, kernel_initializer='he_normal', padding="same")(x)
     x = tf.keras.layers.BatchNormalization()(x)
     
-
     shortcut = inputs
-    #if stride != 1:
     shortcut = tf.keras.layers.Conv2D(filters=filter_num*4, kernel_size=(1, 1), strides=stride, kernel_initializer='he_normal')(inputs)
     shortcut = tf.keras.layers.BatchNormalization()(shortcut)
 
@@ -90,6 +88,6 @@ def make_advanced_block_layer(inputs, filter_num, blocks, stride=1):
     x = make_advanced_block_base(inputs, filter_num, stride=stride)
 
     for _ in range(1, blocks):
-        x = make_basic_block_base(x, filter_num, stride=1)
+        x = make_advanced_block_base(x, filter_num, stride=1)
 
     return x
